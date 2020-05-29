2d0b6eeab8fea07157e31c687bce318b4eda1e92
Chenxu Zhu <cxzhu@pku.edu.cn>
2020-05-29 00:35:09 -0700

    Create 03.proc_RNA.sh

---
+++ shellscrips/03.proc_RNA.sh
@@ -0,0 +1,10 @@
+$s = "input-RNA-fastq-from-step-01"
+mm10="path-to-mm10-STAR-ref"
+mm10_rna="path-to-your-mm10-annotation-reference"
+trim_galore ${s}_BC_cov.fq.gz
+trim_galore -a AAAAAAAAAAAAAAAACCTGCAGGNNNNNNNNNN ${s}_BC_cov_trimmed.fq.gz
+STAR  --runThreadN 6 --genomeDir ${mm10} --readFilesIn ${s}_BC_cov_trimmed_trimmed.fq.gz --readFilesCommand zcat --outFileNamePrefix ${s}_mm10_ --outSAMtype BAM Unsorted
+samtools view -h -F 256 ${s}_mm10_Aligned.out.bam -b > ${s}\_clean.bam
+samtools sort ${s}\_clean.bam ${s}_sorted
+reachtools rmdup2 ${s}_sorted.bam
+reachtools bam2Mtx2 ${s}_sorted_rmdup.bam ${mm10_rna}
