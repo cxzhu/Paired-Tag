175919200bff9cbcf1a6290c19f8fe1828449ab2
Chenxu Zhu <cxzhu@pku.edu.cn>
2020-08-12 16:19:56 -0700

    Update 03.proc_RNA.sh

--- shellscrips/03.proc_RNA.sh
+++ shellscrips/03.proc_RNA.sh
@@ -2,8 +2,8 @@ $s = "input-RNA-fastq-from-step-01"
 mm10="path-to-mm10-STAR-ref"
 mm10_rna="path-to-your-mm10-annotation-reference"
 trim_galore ${s}_BC_cov.fq.gz
-trim_galore -a AAAAAAAAAAAAAAAACCTGCAGGNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN ${s}_BC_cov_trimmed.fq.gz ### trim oligo-dT primer
-trim_galore -a CCTGCAGGNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN ${s}_BC_cov_trimmed.fq.gz ## trim N6 primer
+trim_galore -a AAAAAAAAAAAAAAAACCTGCAGGNNNNACGAATGCTCTGGCCTNNNNNNNNNN ${s}_BC_cov_trimmed.fq.gz ### trim oligo-dT primer
+trim_galore -a CCTGCAGGNNNNACGAATGCTCTGGCCTNNNNNNNNNN ${s}_BC_cov_trimmed.fq.gz ## trim N6 primer
 STAR  --runThreadN 6 --genomeDir ${mm10} --readFilesIn ${s}_BC_cov_trimmed_trimmed_trimmed.fq.gz --readFilesCommand zcat --outFileNamePrefix ${s}_mm10_ --outSAMtype BAM Unsorted
 samtools view -h -F 256 ${s}_mm10_Aligned.out.bam -b > ${s}\_clean.bam
 samtools sort ${s}\_clean.bam ${s}_sorted
