d95e02a892f080cdff0065f94b2c6c3eaa3db3b3
Chenxu Zhu <cxzhu@pku.edu.cn>
2020-12-06 13:13:11 -0800

    Update make.sh

--- reachtools/make.sh
+++ reachtools/make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if g++ -std=c++0x -pthread main.cpp cxstring.cpp readgenome.cpp reachtools.cpp -o ../reachtools
+if g++ -std=c++0x -pthread main.cpp cxstring.cpp readgenome.cpp reachtools.cpp -o ./reachtools
 then
     echo make_finished
 fi
