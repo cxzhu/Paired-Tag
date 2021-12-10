7c09633592b576df9e73874a12450fa065df1d9a
Chenxu Zhu <cxzhu@pku.edu.cn>
2021-12-09 16:22:11 -0800

    Create run.sh

---
+++ remove_pileup/run.sh
@@ -0,0 +1,2 @@
+python count_pileups.py 2020_04_02_Paired-tag_DNA_Active_Merged_sorted.bam_H3K4me1.bam H3K4me1.filtered.pos_strand.tally.txt 
+python remove_pileups.py 2020_04_02_Paired-tag_DNA_Active_Merged_sorted.bam_H3K4me1.bam H3K4me1.filtered.pos_strand.tally.txt H3K4me1_filtered_10.bam 10
