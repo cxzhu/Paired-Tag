92e9842cfd79324b41f925b3cbaefadf42f88432
Chenxu Zhu <cxzhu@pku.edu.cn>
2022-01-26 10:29:22 -0800

    Update 03.proc_RNA.sh

--- shellscrips/03.proc_RNA.sh
+++ shellscrips/03.proc_RNA.sh
@@ -3,7 +3,7 @@ mm10="path-to-mm10-STAR-ref"
 mm10_rna="path-to-your-mm10-annotation-reference"
 trim_galore ${s}_BC_cov.fq.gz
 trim_galore -a AAAAAAAAAAAAAAAACCTGCAGGNNNNACGAATGCTCTGGCCTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN ${s}_BC_cov_trimmed.fq.gz ### trim oligo-dT primer
-trim_galore -a CCTGCAGGNNNNACGAATGCTCTGGCCTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN ${s}_BC_cov_trimmed.fq.gz ## trim N6 primer
+trim_galore -a CCTGCAGGNNNNACGAATGCTCTGGCCTNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN ${s}_BC_cov_trimmed_trimmed.fq.gz ## trim N6 primer
 STAR  --runThreadN 6 --genomeDir ${mm10} --readFilesIn ${s}_BC_cov_trimmed_trimmed_trimmed.fq.gz --readFilesCommand zcat --outFileNamePrefix ${s}_mm10_ --outSAMtype BAM Unsorted
 samtools view -h -F 256 ${s}_mm10_Aligned.out.bam -b > ${s}\_clean.bam
 samtools sort ${s}\_clean.bam ${s}_sorted
