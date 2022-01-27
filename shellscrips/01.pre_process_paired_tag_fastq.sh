5c29cbdf4dc00d1862fc0ab06222d301fb894441
Chenxu Zhu <cxzhu@pku.edu.cn>
2022-01-27 10:55:31 -0800

    Update 01.pre_process_paired_tag_fastq.sh

--- shellscrips/01.pre_process_paired_tag_fastq.sh
+++ shellscrips/01.pre_process_paired_tag_fastq.sh
@@ -1,7 +1,10 @@
 s=$1 # with ${s}_R1.fq.gz and ${s}_R2.fq.gz in the working directory
 p="path-to-cell-id-reference" # build a bowtie index on cell_id_full.fa
 reachtools combine2 ${s}
-zcat ${s}_combined.fq.gz | bowtie ${p} - --norc -m 1 -v 1 -S ${s}_BC.sam
+
+### version for bowtie will chage for use of this step
+zcat ${s}_combined.fq.gz | bowtie ${p} - --norc -m 1 -v 1 -S ${s}_BC.sam ## for bowtie 0.x
+# zcat ${s}_combined.fq.gz | bowtie -x ${p} - --norc -m 1 -v 1 -S ${s}_BC.sam ## for bowtie 1.x
 
 #### This step convert to Celluar Barcode mapped reads to fastq files.
 #### Use this line if for standard fastq files
