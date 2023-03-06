eb41919ee17408e8c81172130286bc99bae7de35
Chenxu Zhu <cxzhu.pku@gmail.com>
2023-03-06 09:20:07 -0500

    Update 01.pre_process_paired_tag_fastq.sh

--- shellscrips/01.pre_process_paired_tag_fastq.sh
+++ shellscrips/01.pre_process_paired_tag_fastq.sh
@@ -1,6 +1,6 @@
 s=$1 # with ${s}_R1.fq.gz and ${s}_R2.fq.gz in the working directory
 p="path-to-cell-id-reference" # build a bowtie index on cell_id_full.fa
-reachtools combine2 ${s}
+reachtools combine2 ${s} ## reachtools combine3 ${s} if use 384-plex format
 
 ### version for bowtie will chage for use of this step
 zcat ${s}_combined.fq.gz | bowtie ${p} - --norc -m 1 -v 1 -S ${s}_BC.sam ## for bowtie 0.x
